074c37a5c2bc1bf972d5138db31543ede722ed42
Ronna Firmo <ronna.firmo1@gmail.com>
2024-10-01 00:14:47 +0800

    chore: add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   define: {
     global: {},
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "icon512_maskable.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "icon512_rounded.png",
+            type: "image/png",
+          },
+        ],
+        orientation: "portrait",
+        display: "standalone",
+        dir: "left",
+        lang: "en-US",
+        name: "ro-pwa",
+        short_name: "ro-pwa",
+        start_url: "/",
+      },
+    }),
+  ],
 });
