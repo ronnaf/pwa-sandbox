83d5196a3589ca981c34a36e9eb7b5c668aaa7fa
Ronna Firmo <ronna.firmo1@gmail.com>
2024-11-21 14:03:43 +0800

    feat: display build information including commit message and SHA in Env component

--- src/Env.tsx
+++ src/Env.tsx
@@ -3,6 +3,15 @@ import { Box } from "./Box";
 
 export interface IEnvProps {}
 
+const buildInfo = {
+  commitMessage: import.meta.env["VITE_VERCEL_GIT_COMMIT_MESSAGE"],
+  commitSha: import.meta.env["VITE_VERCEL_GIT_COMMIT_SHA"],
+};
+
 export const Env = (props: IEnvProps) => {
-  return <Box>{JSON.stringify(import.meta.env, undefined, 2)}</Box>;
+  return <Box>
+    <strong>Version info</strong>
+    <div>Commit: {buildInfo.commitMessage}</div>
+    <div>SHA: {buildInfo.commitSha}</div>
+  </Box>;
 };
