c122b685ce5254a8a7add86daf307cd145feb142
Ronna Firmo <ronna.firmo1@gmail.com>
2024-12-06 23:06:40 +0800

    feat: add event tracking for user interactions in analytics component

--- src/analytics.tsx
+++ src/analytics.tsx
@@ -1,26 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Box } from "./box";
 import Analytiks from "analytics";
 import googleTagManager from "@analytics/google-tag-manager";
 
-const gtm = googleTagManager({ containerId: "GTM-KV6N68MH" })
+const gtm = googleTagManager({ containerId: "GTM-KV6N68MH" });
 console.log(`gtm:`, gtm);
 
 /* Initialize analytics */
 const analytics = Analytiks({
   app: "sandbox",
-  plugins: [
-    gtm 
-  ],
+  plugins: [gtm],
 });
 
 export const Analytics = () => {
-  useEffect(() => {
-    analytics.track("pageview");
-  }, [])
+  function handle() {
+    analytics.track("Whispering secrets", {
+      userId: "12345",
+      name: "John Doe",
+      email: "johndoe@example.com",
+      isActive: true,
+    });
+  }
   return (
     <Box>
       <strong>Analytics</strong>
+      <button onClick={handle}>Whispering secrets</button>
     </Box>
   );
 };
